27f0e7bafdd6340cd6676ea5392b480680dbcb35
ThriveCoder <102660084+ThriveCoder@users.noreply.github.com>
2022-07-26 00:49:57 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// var theDate = new Date();
+// document.getElementById("date").innerHTML = theDate.toLocaleDateString();
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "product.json", true);
+
+// xhr.onload = function () {
+//   if (this.status == 200) {
+//     var users = JSON.parse(this.responseText);
+//     var output = "";
+//     for (var i in users) {
+//       var selectUser = users.spice(0, 2);
+//       console.log(selectUser);
+//       output += selectUser;
+//       document.getElementById("for").innerHTML = output;
+//     }
+//   }
+//   xhr.send();
+// };
+
+// const a = [1, 2, 3];
+// const b = [4, 5, 6];
+
+// const c = a.fill(4, 5, 6);
+
+// document.getElementById("for").innerHTML = c;
